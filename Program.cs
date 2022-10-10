77147528ffeb0297fe5c0a71caf1c937fa74e82d
Pahomix <pahomix@inbox.ru>
2022-10-10 18:45:09 +0300

    2.0

--- Program.cs
+++ Program.cs
@@ -10,99 +10,61 @@ namespace PracticalWork_III
 {
     internal class Program
     {
-        /*static void Buttons()
-        {
-            ConsoleKey[] Buttons = new ConsoleKey[]
-            { ConsoleKey.A, ConsoleKey.W, ConsoleKey.S, ConsoleKey.E, ConsoleKey.D, ConsoleKey.F, ConsoleKey.T, ConsoleKey.G,
-                ConsoleKey.Y, ConsoleKey.H, ConsoleKey.U, ConsoleKey.J, ConsoleKey.K, ConsoleKey.L, ConsoleKey.O, ConsoleKey.P };
-        }*/
-
         static void Octavi(ConsoleKeyInfo F)
         {
-            int[] FivthOctava = new int[]
-                { 523, 554, 587, 622, 659, 699, 740, 784, 831, 880, 932, 988 };
+            int[] SecondOctava = new int[]
+                { 65, 69, 73, 78, 82, 87, 93, 98, 104, 110, 117, 124 };
+            int[] ThirdOctava = new int[12];
+            for (int i = 0; i < ThirdOctava.Length; i++) ThirdOctava[i] = SecondOctava[i] * 2;
+            int[] FourthOctava = new int[12];
+            for (int i = 0; i < FourthOctava.Length; i++) FourthOctava[i] = ThirdOctava[i] * 2;
+            int[] FivthOctava = new int[12];
+            for (int i = 0; i < FivthOctava.Length; i++) FivthOctava[i] = FourthOctava[i] * 2;
             int[] SixthOctava = new int[12];
             for (int i = 0; i < SixthOctava.Length; i++) SixthOctava[i] = FivthOctava[i] * 2;
+            int[] SeventhOctava = new int[12];
+            for (int i = 0; i < SixthOctava.Length; i++) SeventhOctava[i] = SixthOctava[i] * 2;
+            int[] EighthOctava = new int[12];
+            for (int i = 0; i < EighthOctava.Length; i++) EighthOctava[i] = SeventhOctava[i] * 2;
 
             while (true)
             {
                 switch (F.Key)
                 {
+                    case ConsoleKey.F2:
+                        Console.WriteLine("Вторая октава");
+                        Sound(F, SecondOctava);
+                        break;
+                    case ConsoleKey.F3:
+                        Console.WriteLine("Третья октава");
+                        Sound(F, ThirdOctava);
+                        break;
+                    case ConsoleKey.F4:
+                        Console.WriteLine("Четвертая октава");
+                        Sound(F, FourthOctava);
+                        break;
                     case ConsoleKey.F5:
                         Console.WriteLine("Пятая октава");
-                        Sound5(F, FivthOctava);
+                        Sound(F, FivthOctava);
                         break;
                     case ConsoleKey.F6:
                         Console.WriteLine("Шестая октава");
-                        Sound6(F, SixthOctava);
+                        Sound(F, SixthOctava);
+                        break;
+                    case ConsoleKey.F7:
+                        Console.WriteLine("Седьмая октава");
+                        Sound(F, SeventhOctava);
+                        break;
+                    case ConsoleKey.F8:
+                        Console.WriteLine("Восьмая октава");
+                        Sound(F, EighthOctava);
                         break;
                 }
                 if (F.Key == ConsoleKey.Escape) Environment.Exit(0);
             }
         }
 
-        static void Sound5(ConsoleKeyInfo F, int[] FivthOctava)
-         {
-             while (true)
-             {
-                ConsoleKeyInfo note = Console.ReadKey(true);
-                Console.Clear();
-                switch (note.Key)
-                {
-                    case ConsoleKey.A:
-                        Console.Beep(FivthOctava[0], 200);
-                        break;
-                    case ConsoleKey.W:
-                        Console.Beep(FivthOctava[1], 200);
-                        break;
-                    case ConsoleKey.S:
-                        Console.Beep(FivthOctava[2], 200);
-                        break;
-                    case ConsoleKey.E:
-                        Console.Beep(FivthOctava[3], 200);
-                        break;
-                    case ConsoleKey.D:
-                        Console.Beep(FivthOctava[4], 200);
-                        break;
-                    case ConsoleKey.F:
-                        Console.Beep(FivthOctava[5], 200);
-                        break;
-                    case ConsoleKey.T:
-                        Console.Beep(FivthOctava[6], 200);
-                        break;
-                    case ConsoleKey.G:
-                        Console.Beep(FivthOctava[7], 200);
-                        break;
-                    case ConsoleKey.Y:
-                        Console.Beep(FivthOctava[8], 200);
-                        break;
-                    case ConsoleKey.H:
-                        Console.Beep(FivthOctava[9], 200);
-                        break;
-                    case ConsoleKey.U:
-                        Console.Beep(FivthOctava[10], 200);
-                        break;
-                    case ConsoleKey.J:
-                        Console.Beep(FivthOctava[11], 200);
-                        break;
-                    case ConsoleKey.K:
-                        Console.Beep(FivthOctava[0], 200);
-                        break;
-                    case ConsoleKey.L:
-                        Console.Beep(FivthOctava[2], 200);
-                        break;
-                    case ConsoleKey.O:
-                        Console.Beep(FivthOctava[1], 200);
-                        break;
-                    case ConsoleKey.P:
-                        Console.Beep(FivthOctava[3], 200);
-                        break;
-                }
-                if (note.Key == ConsoleKey.F6 || note.Key == ConsoleKey.Escape) Octavi(note);
-             }
-         }
-
-        static void Sound6(ConsoleKeyInfo F, int[] SixthOctava)
+        static void Sound(ConsoleKeyInfo F, int[] Octava)
         {
             while (true)
             {
@@ -111,61 +73,63 @@ namespace PracticalWork_III
                 switch (note.Key)
                 {
                     case ConsoleKey.A:
-                        Console.Beep(SixthOctava[0], 200);
+                        Console.Beep(Octava[0], 200);
                         break;
                     case ConsoleKey.W:
-                        Console.Beep(SixthOctava[1], 200);
+                        Console.Beep(Octava[1], 200);
                         break;
                     case ConsoleKey.S:
-                        Console.Beep(SixthOctava[2], 200);
+                        Console.Beep(Octava[2], 200);
                         break;
                     case ConsoleKey.E:
-                        Console.Beep(SixthOctava[3], 200);
+                        Console.Beep(Octava[3], 200);
                         break;
                     case ConsoleKey.D:
-                        Console.Beep(SixthOctava[4], 200);
+                        Console.Beep(Octava[4], 200);
                         break;
                     case ConsoleKey.F:
-                        Console.Beep(SixthOctava[5], 200);
+                        Console.Beep(Octava[5], 200);
                         break;
                     case ConsoleKey.T:
-                        Console.Beep(SixthOctava[6], 200);
+                        Console.Beep(Octava[6], 200);
                         break;
                     case ConsoleKey.G:
-                        Console.Beep(SixthOctava[7], 200);
+                        Console.Beep(Octava[7], 200);
                         break;
                     case ConsoleKey.Y:
-                        Console.Beep(SixthOctava[8], 200);
+                        Console.Beep(Octava[8], 200);
                         break;
                     case ConsoleKey.H:
-                        Console.Beep(SixthOctava[9], 200);
+                        Console.Beep(Octava[9], 200);
                         break;
                     case ConsoleKey.U:
-                        Console.Beep(SixthOctava[10], 200);
+                        Console.Beep(Octava[10], 200);
                         break;
                     case ConsoleKey.J:
-                        Console.Beep(SixthOctava[11], 200);
+                        Console.Beep(Octava[11], 200);
                         break;
                     case ConsoleKey.K:
-                        Console.Beep(SixthOctava[0], 200);
+                        Console.Beep(Octava[0], 200);
                         break;
                     case ConsoleKey.L:
-                        Console.Beep(SixthOctava[2], 200);
+                        Console.Beep(Octava[2], 200);
                         break;
                     case ConsoleKey.O:
-                        Console.Beep(SixthOctava[1], 200);
+                        Console.Beep(Octava[1], 200);
                         break;
                     case ConsoleKey.P:
-                        Console.Beep(SixthOctava[3], 200);
+                        Console.Beep(Octava[3], 200);
                         break;
                 }
-                if (note.Key == ConsoleKey.F5 || note.Key == ConsoleKey.Escape) Octavi(note);
+                if (note.Key == ConsoleKey.F2 || note.Key == ConsoleKey.F3 || note.Key == ConsoleKey.Escape ||
+                note.Key == ConsoleKey.F4 || note.Key == ConsoleKey.F5 || note.Key == ConsoleKey.F6 ||
+                note.Key == ConsoleKey.F7 || note.Key == ConsoleKey.F8) Octavi(note);
             }
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Выберите октаву(F5-F6)");
+            Console.WriteLine("Выберите октаву(F2-F8)");
             ConsoleKeyInfo F = Console.ReadKey(true);
             Octavi(F);
         }
